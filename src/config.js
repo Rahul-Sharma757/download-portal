74103e289ecefb7d9c1d180921d459efa1468fcf
kulwantsingh696 <kulwant.prlxweb@gmail.com>
2023-06-13 14:11:58 +0530

    updated API Url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     //baseUrl: 'https://api.activate.smallhd.com/apis/',
-    apiUrl: 'https://downloads.smallhd.com:4002', // https://downloads.smallhd.com:4002, http://localhost:4002
+    apiUrl: 'https://repairs.cs.inc:7010', // https://downloads.smallhd.com:4002, http://localhost:4002
     siteUrl: 'https://downloads.smallhd.com', //https://downloads.smallhd.com/ , http://localhost:3000
     downloadFirmwareUrl : 'https://downloads.smallhd.com/dev/backend-node-server/firmware/',//smallhd_OS3_firmware
     itemsimgUrl : 'https://cdn.shopify.com/s/files/1/0097/6004/7167/products/',
